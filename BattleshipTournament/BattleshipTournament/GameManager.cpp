7aaf474711f2f62960eeedaf41ab45847f1fe5bd
ewaserma <eyal.waserman@intel.com>
2017-06-10 18:25:49 +0300

    fix compilation after previous commit

--- BattleshipTournament/BattleshipTournament/GameManager.cpp
+++ BattleshipTournament/BattleshipTournament/GameManager.cpp
@@ -199,25 +199,8 @@ brd(Board(tm.getBoard(ge.board_inx)))
     players[PLAYER_A].id = 'A';
     players[PLAYER_B].id = 'B';
 
-    bool first_smaller = (ge.players_indices.first < ge.players_indices.second);
-    if (ge.A_is_smaller_B_is_larger)
-    {
-        players[PLAYER_A].algo = (first_smaller) ? 
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.first).first()) :
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.second).first());
-        players[PLAYER_B].algo = (first_smaller) ?
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.second).first()) :
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.first).first());
-    }
-    else
-    {
-        players[PLAYER_A].algo = (first_smaller) ?
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.second).first()) :
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.first).first());
-        players[PLAYER_B].algo = (first_smaller) ?
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.first).first()) :
-            unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.second).first());
-    }
+    players[PLAYER_A].algo = unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.first).first());
+    players[PLAYER_B].algo = unique_ptr<IBattleshipGameAlgo>(tm.getAlgo(ge.players_indices.second).first());
 }
 
 //user should be 'A' or 'B'
